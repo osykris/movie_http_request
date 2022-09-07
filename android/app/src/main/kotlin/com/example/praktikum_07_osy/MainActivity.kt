2ffb5cd820ad9946a6beb61cb3bf2b1f2d8acead
osykris <osy.krisdayanti27@gmail.com>
2022-09-07 14:39:59 +0700

    create flutter new project

---
+++ android/app/src/main/kotlin/com/example/praktikum_07_osy/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.praktikum_07_osy
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:praktikum_07_osy/pages/movie_list.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+          primarySwatch: Colors.blueGrey, backgroundColor: Colors.grey),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MovieList();
+  }
+}
